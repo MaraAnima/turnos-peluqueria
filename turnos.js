d0be185e58b94cf09b6a0b97a64ccfc3430792cd
Mara <mara.duarte@anima.edu.uy>
2026-04-14 10:30:21 -0300

    Update turnos.js

--- turnos.js
+++ turnos.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const URL =
-    "https://script.google.com/macros/s/AKfycbxOBwLYf4s9Q78yGvD65MIkscDbVpde4cApVm0ZYx2MRa3bNINA-BS-DASXaYNns-sICg/exec";
+    "https://script.google.com/macros/s/AKfycbxGRyB8GXY1sTHn-5F5ekpAs2stzNODq6vcWqYuyeRHUEakL5m2Dok2uEkMisUc2uTN/exec";
   const horariosBase = ["12:00", "14:00", "16:00", "18:00"];
 
   const app = document.getElementById("app-turnos");
